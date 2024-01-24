d2bc7a5a9109950787ad9e4d2fa7a376263a22b3
Manjula Pradeep <manjulapradeep057@gmail.com>
2024-01-24 17:06:30 +0530

    Intergration of "CoinGecko" public API

--- src/v1/controllers/coinController.js
+++ src/v1/controllers/coinController.js
@@ -2,15 +2,42 @@ const axios = require("axios");
 
 class CoinController {
   async getCoinList(req, res) {
+
+    const url1 = `https://api.coingecko.com/api/v3/coins/list`;
     try {
-      const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/list"
-      );
+      const response = await axios.get(url1);
       const coinList = response.data;
       res.json(coinList);
     } catch (error) {
       console.error("Error fetching coin list:", error.message);
-      res.status(500).json({ message: 'Internal Server Error', error: error.message || error });
+      res.status(500).json({
+        message: "Internal Server Error",
+        error: error.message || error,
+      });
+    }
+  }
+
+  async getPriceList(req, res) {
+    const vs_currency = req.params.vs_currency || "usd";
+    const order = req.params.order || "market_cap_desc";
+    const per_page = req.params.per_page || "100";
+    const page = req.params.page || 1;
+    const sparkline = req.params.sparkline || false;
+    const locale = req.params.locale || "en";
+
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}&sparkline=${sparkline}&locale=${locale}`;
+
+    try {
+      const response = await axios.get(url);
+      const priceList = response.data;
+      res.status(200).json(priceList);
+
+    } catch (error) {
+      console.error("Error fetching coin list:", error.message);
+      res.status(500).json({
+        message: "Internal Server Error",
+        error: error.message || error,
+      });
     }
   }
 }
--- src/v1/routes/coinRouter.js
+++ src/v1/routes/coinRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const coinController = require('../controllers/coinController');
 
 router.get('/coinList', coinController.getCoinList);
+router.get('/priceList', coinController.getPriceList);
 
 module.exports = router;

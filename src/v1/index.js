38a7e77e778da84234a8bf1312f432196927a751
Manjula Pradeep <manjulapradeep057@gmail.com>
2024-01-28 22:30:15 +0530

    web socket creation

--- src/v1/index.js
+++ src/v1/index.js
@@ -13,55 +13,55 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = require("../v1/swagger/swaggerOptions");
 const config = require("../v1/config/config");
 const http = require("http");
+const path = require("path");
 
-// const WebSocket = require("ws");
-// const fs = require("fs");
-// const path = require("path");
+const WebSocket = require("ws");
+const fs = require("fs");
+
 
 const app = express();
 const port = config.PORT || 3000;
 
 app.get("/", (req, res) => {
-  const responseMessage = 'index.js is running....<br>API Documentation - <a href="http://localhost:3000/api-docs/">http://localhost:3000/api-docs/</a>';
-  res.send(responseMessage);
-
+  res.sendFile('index.html', { root: path.join(__dirname,) });
 });
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// // WebSocket handling
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server, path: "/cryptoprices" });
-// const clients = new Set();
+// WebSocket handling
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server, path: "/cryptoprices" });
+const clients = new Set();
 
-// function broadcastPrices(prices) {
-//   const serializedPrices = JSON.stringify(prices);
-//   clients.forEach((client) => {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(serializedPrices);
-//     }
-//   });
-// }
+function broadcastPrices(prices) {
+  const serializedPrices = JSON.stringify(prices);
+  clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(serializedPrices); 
+    }
+  });
+}
 
-// wss.on("connection", (ws) => {
-//   clients.add(ws);
-//   ws.on("message", (message) => {
-//     console.log(`Received message: ${message}`);
-//   });
-//   ws.on("close", () => {
-//     clients.delete(ws);
-//   });
-// });
+wss.on("connection", (ws) => {
+  clients.add(ws);
+  ws.on("message", (message) => {
+    console.log(`Received message: ${message}`);
+  });
+  ws.on("close", () => {
+    clients.delete(ws);
+  });
+});
 
-// // Swagger API documentation
-// // http://localhost:3000/api-docs/
+
+// Swagger API documentation http://localhost:3000/api-docs/
 // Initialize Swagger-jsdoc
 const specs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/coins", verifyUserToken, coinRouter);
+// app.use("/api/v1/coins", verifyUserToken, coinRouter);
+app.use("/api/v1/coins", coinRouter);
 app.use("/api/v1/userFavorites", verifyUserToken, userFavoritesRouter);
 
 app.use((err, req, res, next) => {
@@ -73,24 +73,18 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// // Fetch and broadcast prices every 5 seconds
-// async function fetchAndBroadcastPrices() {
-//     try {
-//         const cryptos = ['bitcoin', 'ethereum', 'ripple'];
-//         const currency = 'USD';
-//         const options = {
-//             order: 'market_cap_desc',
-//             per_page: 100,
-//             page: 1,
-//             sparkline: false,
-//             locale: 'en',
-//         };
+// Fetch and broadcast prices every 5 minutes
+async function fetchAndBroadcastPrices() {
+  try {
+    // Fetch prices from the new getPriceList route
+    const response = await axios.get('http://localhost:3000/api/v1/coins/priceList');
+    const prices = response.data;
 
-//         const prices = await cryptoService.getCryptoPrices(cryptos, currency, options);
-//         broadcastPrices(prices);
-//     } catch (error) {
-//         console.error('Error fetching and broadcasting prices:', error.message);
-//     }
-// }
+    // Broadcast prices to WebSocket clients
+    broadcastPrices(prices);
+  } catch (error) {
+    console.error('Error fetching and broadcasting prices:', error.message);
+  }
+}
 
-// setInterval(fetchAndBroadcastPrices, 300000);
+setInterval(fetchAndBroadcastPrices, 300000);
